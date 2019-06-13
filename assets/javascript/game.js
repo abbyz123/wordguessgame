30b7161e60c24cf4a20292120824ccfb72444a46
mengyuan zhang <zhangm8976@student.laccd.edu>
2019-06-12 18:54:31 -0700

    add more guessing word. Add hash table for word letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,16 @@
 // global variables
-let wordGuess = ["Lannister", "Stark", "Tangaryen", "Baratheon"];    // Guessing words
-let imageLoc = "../images/";                                         // image location
-let wordImage = [null, null, null, null];                            // Image to show after a word is correctly guessed
-let wordMusic = [null, null, null, null];                            // Music to play after a word is correctly guessed
+let wordGuess = ["Lannister", "Stark", "Tangaryen", "Baratheon", "Cercei", "Arya", "Jon", "Robb", "Daenerys", "Sansa"];    // Guessing words
+let imageLoc = "../images/";                                                                                               // image location
+let wordImage = [null, null, null, null, null, null, null, null, null, null];                                              // Image to show after a word is correctly guessed
+let wordMusic = [null, null, null, null, null, null, null, null, null, null];                                              // Music to play after a word is correctly guessed
 let winNum = 0;
 let guessNum = 12;
 
 // Game Object
 let currentGame = Object();
+
+// word letter hash table
+let wordTable = Object();
 
 // Functions
 function gameStart() {
@@ -22,4 +25,15 @@ function gameStart() {
     document.getElementById("wins").innerHTML = "Win: " + winNum;   // Initialize number of wins
     document.getElementById("wordBlank").innerHTML = "_ ".repeat(currentGame.word.length);   // generate blanks according to the word length
     document.getElementById("guessNum").innerHTML = guessNum + " of guess remains"; // number of guess remains
+
+    // initialize the word letter hash table
+    for (i = 0; i < currentGame.word.length; i++) {
+        currLetter = currentGame.word[i].toUpperCase();
+        if (currLetter in wordTable) {
+            wordTable[currLetter].push(i);
+        } else {
+            wordTable[currLetter] = [i];
+        }
+    }
 }
+
